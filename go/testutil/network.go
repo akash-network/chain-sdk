41a21ef4c98fbdf167f32bbb2c57a49fadf0e12a
Artur Troian <2477474+troian@users.noreply.github.com>
2025-08-06 10:41:12 -0500

    feat(go/testutil): implement free port allocator (#17)
    Signed-off-by: Artur Troian <troian@users.noreply.github.com>
    Co-authored-by: Artur Troian <troian@users.noreply.github.com>

--- go/testutil/network.go
+++ go/testutil/network.go
@@ -1,1 +1,62 @@
 package testutil
+
+import (
+	"net"
+	"sync"
+)
+
+// GetFreePorts asks the kernel for free open ports that are ready to use.
+func GetFreePorts(count int) ([]int, error) {
+	var ports []int
+	listeners := make([]*net.TCPListener, 0, count)
+
+	defer func() {
+		for _, l := range listeners {
+			_ = l.Close()
+		}
+	}()
+
+	for i := 0; i < count; i++ {
+		addr, err := net.ResolveTCPAddr("tcp", "localhost:0")
+		if err != nil {
+			return nil, err
+		}
+
+		l, err := net.ListenTCP("tcp", addr)
+		if err != nil {
+			return nil, err
+		}
+
+		listeners = append(listeners, l)
+		ports = append(ports, l.Addr().(*net.TCPAddr).Port)
+	}
+
+	return ports, nil
+}
+
+type freePorts struct {
+	lock  sync.Mutex
+	idx   int
+	ports []int
+}
+
+func newFreePorts(ports []int) *freePorts {
+	return &freePorts{
+		idx:   0,
+		ports: ports,
+	}
+}
+
+func (p *freePorts) mustGetPort() int {
+	defer p.lock.Unlock()
+	p.lock.Lock()
+
+	if p.idx == len(p.ports) {
+		panic("no ports available")
+	}
+
+	port := p.ports[p.idx]
+	p.idx++
+
+	return port
+}
