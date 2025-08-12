8acf339aa5966e93e2e45f2f3aebc6344c63fa86
Artur Troian <troian@users.noreply.github.com>
2025-08-12 08:48:58 -0500

    feat(go/util/ctxlog): implement log from context
    migrate logger to cosmossdk.io/log
    
    Signed-off-by: Artur Troian <troian@users.noreply.github.com>

--- go/util/ctxlog/ctxlog.go
+++ go/util/ctxlog/ctxlog.go
@@ -3,17 +3,18 @@ package ctxlog
 import (
 	"context"
 
-	"github.com/cometbft/cometbft/libs/log"
+	"cosmossdk.io/log"
+	"github.com/go-logr/logr"
 )
 
-type ctxKey string
+type CtxKey string
 
 const (
-	ctxKeyLog = "log"
+	CtxKeyLog = CtxKey("log")
 )
 
 type options struct {
-	logName ctxKey
+	logName CtxKey
 }
 
 type LogOption func(*options) error
@@ -21,7 +22,7 @@ type LogOption func(*options) error
 // WithLogName set custom name of the log object
 func WithLogName(val string) LogOption {
 	return func(t *options) error {
-		t.logName = ctxKey(val)
+		t.logName = CtxKey(val)
 		return nil
 	}
 }
@@ -41,8 +42,37 @@ func WithLogger(ctx context.Context, lg log.Logger, opts ...LogOption) context.C
 	return ctx
 }
 
+// WithLogc add logger object to the context
+// key defaults to the "log"
+// use WithLogName("<custom name>") to set custom key
+func WithLogc(ctx context.Context, lg log.Logger, opts ...LogOption) context.Context {
+	opt, _ := applyOptions(opts...)
+
+	ctx = context.WithValue(ctx, opt.logName, lg)
+
+	return ctx
+}
+
+func LogcFromCtx(ctx context.Context, opts ...LogOption) log.Logger {
+	opt, _ := applyOptions(opts...)
+
+	var logger log.Logger
+	if lg, valid := ctx.Value(opt.logName).(log.Logger); valid {
+		logger = lg
+	} else {
+		logger = &dummyLogger{}
+	}
+
+	return logger
+}
+
+func LogrFromCtx(ctx context.Context) logr.Logger {
+	lg, _ := logr.FromContext(ctx)
+	return lg
+}
+
 // Logger get logger from the context.
-// By default it uses "log" key. use WithLogName("<custom name>") to set custom key
+// By default, it uses "log" key. use WithLogName("<custom name>") to set custom key
 // If logger not found dummyLogger is returned
 func Logger(ctx context.Context, opts ...LogOption) log.Logger {
 	opt, _ := applyOptions(opts...)
@@ -55,6 +85,13 @@ func Logger(ctx context.Context, opts ...LogOption) log.Logger {
 	}
 
 	return logger
+}
+
+func (l *dummyLogger) Warn(_ string, _ ...any) {
+}
+
+func (l *dummyLogger) Impl() any {
+	return l
 }
 
 func (l *dummyLogger) Debug(_ string, _ ...interface{}) {}
@@ -73,7 +110,7 @@ func applyOptions(opts ...LogOption) (options, error) {
 	}
 
 	if obj.logName == "" {
-		obj.logName = ctxKeyLog
+		obj.logName = CtxKeyLog
 	}
 
 	return *obj, nil
