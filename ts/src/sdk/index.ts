505d647727e5915f00cef183d550551a91f217c1
Sergii <sergiy.stotskiy@gmail.com>
2025-10-13 14:50:26 +0300

    fix: updates exports

--- ts/src/sdk/index.ts
+++ ts/src/sdk/index.ts
@@ -2,4 +2,4 @@ export * from "./index.shared.ts";
 export { createChainNodeSDK, type ChainNodeSDKOptions } from "./chain/createChainNodeSDK.ts";
 export { createProviderSDK, type ProviderSDKOptions } from "./provider/createProviderSDK.ts";
 export { certificateManager, CertificateManager, type CertificateInfo, type CertificatePem, type ValidityRangeOptions } from "./provider/auth/mtls/index.ts";
-export { JwtTokenManager, type CreateJWTOptions, type JwtTokenPayload, type JwtValidationResult, createSignArbitraryAkashWallet, type SignArbitraryAkashWallet } from "./provider/auth/jwt/index.ts";
+export * from "./provider/auth/jwt/index.ts";
