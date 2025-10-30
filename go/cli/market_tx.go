166b4375321668671fb986f1889e88a8417104f1
Joao Luna <7607329+cloud-j-luna@users.noreply.github.com>
2025-10-30 01:49:13 +0000

    fix(go/cli): add close reason flag to lease and bid close (#154)
    Signed-off-by: Artur Troian <troian@users.noreply.github.com>
    Co-authored-by: Artur Troian <troian@users.noreply.github.com>

--- go/cli/market_tx.go
+++ go/cli/market_tx.go
@@ -115,8 +115,14 @@ func GetTxMarketBidCloseCmd() *cobra.Command {
 				return err
 			}
 
+			reason, err := cflags.BidClosedReasonFromFlags(cmd.Flags())
+			if err != nil {
+				return err
+			}
+
 			msg := &types.MsgCloseBid{
-				ID: id,
+				ID:     id,
+				Reason: reason,
 			}
 
 			if err := msg.ValidateBasic(); err != nil {
@@ -134,6 +140,7 @@ func GetTxMarketBidCloseCmd() *cobra.Command {
 
 	cflags.AddTxFlagsToCmd(cmd)
 	cflags.AddBidIDFlags(cmd.Flags())
+	cflags.AddBidClosedReasonFlag(cmd.Flags())
 
 	return cmd
 }
@@ -252,8 +259,10 @@ func GetTxMarketLeaseCloseCmd() *cobra.Command {
 				return err
 			}
 
+			// for lease closed tx reason is always owner
 			msg := &types.MsgCloseLease{
-				ID: id,
+				ID:     id,
+				Reason: mtypes.LeaseClosedReasonOwner,
 			}
 
 			if err := msg.ValidateBasic(); err != nil {
