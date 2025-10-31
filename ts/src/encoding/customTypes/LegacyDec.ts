b8a23b9fcf61bc46ffdf9c92dc68820fd2512553
Sergii <sergiy.stotskiy@gmail.com>
2025-10-31 09:42:33 +0100

    fix(ts): decodes/encodes negative LegacyDec values

--- ts/src/encoding/customTypes/LegacyDec.ts
+++ ts/src/encoding/customTypes/LegacyDec.ts
@@ -10,6 +10,21 @@ const PRECISION = 18;
 export const LegacyDec = {
   typeName: "cosmossdk.io/math.LegacyDec",
   shortName: "LegacyDec",
-  encode: (value: string) => value.length ? Decimal.fromUserInput(value, PRECISION).atomics : "",
-  decode: (value: string) => value.length ? Decimal.fromAtomics(value, PRECISION).toString() : "",
+  encode(value: string) {
+    if (!value.length) return "";
+    const { sign, value: positiveValue } = unsignedDecimal(value);
+    return sign + Decimal.fromUserInput(positiveValue, PRECISION).atomics;
+  },
+  decode(value: string) {
+    if (!value.length) return "";
+    const { sign, value: positiveValue } = unsignedDecimal(value);
+    return sign + Decimal.fromAtomics(positiveValue, PRECISION).toString();
+  },
 } as const satisfies CustomType<string, string>;
+
+// cosmjs Decimal supports only non-negative decimals: https://github.com/cosmos/cosmjs/issues/1897
+function unsignedDecimal(value: string) {
+  if (value[0] !== "-") return { sign: "", value };
+  const positiveValue = value.slice(1);
+  return { sign: "-", value: positiveValue };
+}
