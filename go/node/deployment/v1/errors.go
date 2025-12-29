dc80e0daed3299bd65fa3eb11126e7fde91f97dc
Artem Shcherbatiuk <vertex451@gmail.com>
2025-12-29 16:23:14 +0100

    fix[go]: replaced cerrors.Register with cerrors.RegisterWithGRPCCode to propagate the proper http code into the response

--- go/node/deployment/v1/errors.go
+++ go/node/deployment/v1/errors.go
@@ -2,6 +2,7 @@ package v1
 
 import (
 	cerrors "cosmossdk.io/errors"
+	"google.golang.org/grpc/codes"
 )
 
 const (
@@ -32,48 +33,49 @@ const (
 
 var (
 	// ErrNameDoesNotExist is the error when name does not exist
-	ErrNameDoesNotExist = cerrors.Register(ModuleName, errNameDoesNotExist, "Name does not exist")
+	ErrNameDoesNotExist = cerrors.RegisterWithGRPCCode(ModuleName, errNameDoesNotExist, codes.NotFound, "Name does not exist")
 	// ErrInvalidRequest is the error for invalid request
-	ErrInvalidRequest = cerrors.Register(ModuleName, errInvalidRequest, "Invalid request")
+	ErrInvalidRequest = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidRequest, codes.InvalidArgument, "Invalid request")
 	// ErrDeploymentExists is the error when already deployment exists
-	ErrDeploymentExists = cerrors.Register(ModuleName, errDeploymentExists, "Deployment exists")
+	ErrDeploymentExists = cerrors.RegisterWithGRPCCode(ModuleName, errDeploymentExists, codes.AlreadyExists, "Deployment exists")
 	// ErrDeploymentNotFound is the error when deployment not found
-	ErrDeploymentNotFound = cerrors.Register(ModuleName, errDeploymentNotFound, "Deployment not found")
+	ErrDeploymentNotFound = cerrors.RegisterWithGRPCCode(ModuleName, errDeploymentNotFound, codes.NotFound, "Deployment not found")
 	// ErrDeploymentClosed is the error when deployment is closed
-	ErrDeploymentClosed = cerrors.Register(ModuleName, errDeploymentClosed, "Deployment closed")
+	ErrDeploymentClosed = cerrors.RegisterWithGRPCCode(ModuleName, errDeploymentClosed, codes.FailedPrecondition, "Deployment closed")
 	// ErrOwnerAcctMissing is the error for owner account missing
-	ErrOwnerAcctMissing = cerrors.Register(ModuleName, errOwnerAcctMissing, "Owner account missing")
+	ErrOwnerAcctMissing = cerrors.RegisterWithGRPCCode(ModuleName, errOwnerAcctMissing, codes.InvalidArgument, "Owner account missing")
 	// ErrInvalidGroups is the error when groups are empty
-	ErrInvalidGroups = cerrors.Register(ModuleName, errInvalidGroups, "Invalid groups")
+	ErrInvalidGroups = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidGroups, codes.InvalidArgument, "Invalid groups")
 	// ErrInvalidDeploymentID is the error for invalid deployment id
-	ErrInvalidDeploymentID = cerrors.Register(ModuleName, errInvalidDeploymentID, "Invalid: deployment id")
+	ErrInvalidDeploymentID = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidDeploymentID, codes.InvalidArgument, "Invalid: deployment id")
 	// ErrEmptyHash is the error when version is empty
-	ErrEmptyHash = cerrors.Register(ModuleName, errEmptyHash, "Invalid: empty hash")
+	ErrEmptyHash = cerrors.RegisterWithGRPCCode(ModuleName, errEmptyHash, codes.InvalidArgument, "Invalid: empty hash")
 	// ErrInvalidHash is the error when version is invalid
-	ErrInvalidHash = cerrors.Register(ModuleName, errInvalidHash, "Invalid: deployment hash")
+	ErrInvalidHash = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidHash, codes.InvalidArgument, "Invalid: deployment hash")
 	// ErrInternal is the error for internal error
-	ErrInternal = cerrors.Register(ModuleName, errInternal, "internal error")
+	ErrInternal = cerrors.RegisterWithGRPCCode(ModuleName, errInternal, codes.Internal, "internal error")
 	// ErrInvalidDeployment = is the error when deployment does not pass validation
-	ErrInvalidDeployment = cerrors.Register(ModuleName, errInvalidDeployment, "Invalid deployment")
-	// ErrInvalidGroupID is the error when already deployment exists
-	ErrInvalidGroupID = cerrors.Register(ModuleName, errInvalidGroupID, "Deployment exists")
+	ErrInvalidDeployment = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidDeployment, codes.InvalidArgument, "Invalid deployment")
+	// ErrInvalidGroupID is the error when the deployment's group ID is invalid
+	ErrInvalidGroupID = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidGroupID, codes.InvalidArgument, "Invalid deployment's group ID")
 	// ErrGroupNotFound is the keeper's error for not finding a group
-	ErrGroupNotFound = cerrors.Register(ModuleName, errGroupNotFound, "Group not found")
+	ErrGroupNotFound = cerrors.RegisterWithGRPCCode(ModuleName, errGroupNotFound, codes.NotFound, "Group not found")
 	// ErrGroupClosed is the error when deployment is closed
-	ErrGroupClosed = cerrors.Register(ModuleName, errGroupClosed, "Group already closed")
+	ErrGroupClosed = cerrors.RegisterWithGRPCCode(ModuleName, errGroupClosed, codes.FailedPrecondition, "Group already closed")
 	// ErrGroupOpen is the error when deployment is closed
-	ErrGroupOpen = cerrors.Register(ModuleName, errGroupOpen, "Group open")
+	ErrGroupOpen = cerrors.RegisterWithGRPCCode(ModuleName, errGroupOpen, codes.FailedPrecondition, "Group open")
 	// ErrGroupPaused is the error when deployment is closed
-	ErrGroupPaused = cerrors.Register(ModuleName, errGroupPaused, "Group paused")
+	ErrGroupPaused = cerrors.RegisterWithGRPCCode(ModuleName, errGroupPaused, codes.FailedPrecondition, "Group paused")
 	// ErrGroupNotOpen indicates the Group state has progressed beyond initial Open.
-	ErrGroupNotOpen = cerrors.Register(ModuleName, errGroupNotOpen, "Group not open")
+	ErrGroupNotOpen = cerrors.RegisterWithGRPCCode(ModuleName, errGroupNotOpen, codes.FailedPrecondition, "Group not open")
 	// ErrGroupSpecInvalid indicates a GroupSpec has invalid configuration
-	ErrGroupSpecInvalid = cerrors.Register(ModuleName, errGroupSpec, "GroupSpec invalid")
+	ErrGroupSpecInvalid = cerrors.RegisterWithGRPCCode(ModuleName, errGroupSpec, codes.InvalidArgument, "GroupSpec invalid")
 	// ErrInvalidDeposit indicates an invalid deposit
-	ErrInvalidDeposit = cerrors.Register(ModuleName, errInvalidDeposit, "Deposit invalid")
+	ErrInvalidDeposit = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidDeposit, codes.InvalidArgument, "Deposit invalid")
 	// ErrInvalidIDPath indicates an invalid ID path
-	ErrInvalidIDPath = cerrors.Register(ModuleName, errInvalidIDPath, "ID path invalid")
+	ErrInvalidIDPath = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidIDPath, codes.InvalidArgument, "ID path invalid")
 	// ErrInvalidParam indicates an invalid chain parameter
-	ErrInvalidParam    = cerrors.Register(ModuleName, errInvalidParam, "parameter invalid")
-	ErrInvalidEscrowID = cerrors.Register(ModuleName, errInvalidEscrowID, "invalid escrow id")
+	ErrInvalidParam = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidParam, codes.InvalidArgument, "parameter invalid")
+	// ErrInvalidEscrowID indicates an invalid escrow ID
+	ErrInvalidEscrowID = cerrors.RegisterWithGRPCCode(ModuleName, errInvalidEscrowID, codes.InvalidArgument, "invalid escrow id")
 )
