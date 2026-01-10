84f2918ca07ba133ac07731ee3d6fa09715811f5
Serhii Stotskyi <serhii@akash.network>
2026-01-10 07:01:29 +0100

    chore(ts): configures automated version releasing (#178)

--- make/release-ts.mk
+++ make/release-ts.mk
@@ -1,6 +1,6 @@
 .PHONY: release-ts
 release-ts: $(AKASH_TS_NODE_MODULES) $(AKASH_TS_ROOT)/dist
-	script/release-ts.sh
+	cd $(AKASH_TS_ROOT) && npm run release
 
 $(AKASH_TS_ROOT)/dist: $(shell find $(AKASH_TS_ROOT)/src -type f)
 	cd $(AKASH_TS_ROOT) && npm run build
