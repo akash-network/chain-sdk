6b60fb0ba1c37fb7bc265626f47eb9be1d552b43
Artur Troian <2477474+troian@users.noreply.github.com>
2026-01-12 07:39:27 -0600

    feat(go): add testutil v1beta4 (#186)
    Signed-off-by: Artur Troian <troian@users.noreply.github.com>
    Co-authored-by: Artur Troian <troian@users.noreply.github.com>

---
+++ go/testutil/v1beta4/sdk.go
@@ -0,0 +1,62 @@
+package testutil
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	cmbtypes "github.com/cometbft/cometbft/abci/types"
+
+	sdkmath "cosmossdk.io/math"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/gogoproto/proto"
+)
+
+func Coin(t testing.TB) sdk.Coin {
+	t.Helper()
+	return sdk.NewCoin("testcoin", sdkmath.NewInt(int64(RandRangeInt(1, 1000)))) // nolint: gosec
+}
+
+func DecCoin(t testing.TB) sdk.DecCoin {
+	t.Helper()
+	return sdk.NewDecCoin("testcoin", sdkmath.NewInt(int64(RandRangeInt(1, 1000)))) // nolint: gosec
+}
+
+// AkashCoinRandom provides simple interface to the Akash sdk.Coin type.
+func AkashCoinRandom(t testing.TB) sdk.Coin {
+	t.Helper()
+	amt := sdkmath.NewInt(int64(RandRangeInt(1, 1000)))
+	return sdk.NewCoin(CoinDenom, amt)
+}
+
+// AkashCoin provides simple interface to the Akash sdk.Coin type.
+func AkashCoin(t testing.TB, amount int64) sdk.Coin {
+	t.Helper()
+	amt := sdkmath.NewInt(amount)
+	return sdk.NewCoin(CoinDenom, amt)
+}
+
+func AkashDecCoin(t testing.TB, amount int64) sdk.DecCoin {
+	t.Helper()
+	amt := sdkmath.NewInt(amount)
+	return sdk.NewDecCoin(CoinDenom, amt)
+}
+
+func AkashDecCoinRandom(t testing.TB) sdk.DecCoin {
+	t.Helper()
+	amt := sdkmath.NewInt(int64(RandRangeInt(1, 1000)))
+	return sdk.NewDecCoin(CoinDenom, amt)
+}
+
+func EnsureEvent(t *testing.T, events []cmbtypes.Event, expEvent proto.Message) {
+	for _, e := range events {
+		iev, err := sdk.ParseTypedEvent(e)
+		require.NoError(t, err)
+		if reflect.DeepEqual(iev, expEvent) {
+			return
+		}
+	}
+
+	t.Errorf("events don't have required event \"%v\"", expEvent)
+}
