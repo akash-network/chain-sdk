24172eec75a6369f0d85b5c037bd151ceae0d22f
Artem Shcherbatiuk <vertex451@gmail.com>
2026-03-31 19:12:04 +0200

    chore: authz_tests

--- go/node/escrow/v1/authz_test.go
+++ go/node/escrow/v1/authz_test.go
@@ -3,67 +3,410 @@ package v1_test
 import (
 	"testing"
 
-	"github.com/stretchr/testify/require"
-
+	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	"github.com/cosmos/cosmos-sdk/x/authz"
+	"github.com/stretchr/testify/require"
 
+	dvbeta "pkg.akt.dev/go/node/deployment/v1beta4"
 	v1 "pkg.akt.dev/go/node/escrow/v1"
+	"pkg.akt.dev/go/node/escrow/module"
+	mvbeta "pkg.akt.dev/go/node/market/v1beta5"
 	deposit "pkg.akt.dev/go/node/types/deposit/v1"
 	"pkg.akt.dev/go/sdkutil"
 	"pkg.akt.dev/go/testutil"
 )
 
-func TestDepositAuthorizationAccept(t *testing.T) {
-	limit := sdk.NewInt64Coin(sdkutil.DenomUact, 333)
-	dda := v1.NewDepositAuthorization(v1.DepositAuthorizationScopes{v1.DepositScopeDeployment}, sdk.Coins{limit})
+func TestDepositAuthorization_TryAccept(t *testing.T) {
+	denom := sdkutil.DenomUact
+	limit := sdk.NewInt64Coin(denom, 1000)
+	dep := func(amount sdk.Coin) deposit.Deposit {
+		return deposit.Deposit{
+			Amount:  amount,
+			Sources: deposit.Sources{deposit.SourceGrant},
+		}
+	}
 
-	sctx := sdk.Context{}
+	tests := []struct {
+		name       string
+		scopes     v1.DepositAuthorizationScopes
+		limits     sdk.Coins
+		msg        sdk.Msg
+		partial    bool
+		expAccept  bool
+		expDelete  bool
+		expErr     error
+		expErrMsg  string
+		expUpdated bool
+	}{
+		{
+			name:      "nil_msg",
+			scopes:    v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits:    sdk.Coins{limit},
+			msg:       nil,
+			expErr:    sdkerrors.ErrInvalidType,
+			expErrMsg: "msg cannot be nil",
+		},
+		{
+			name:   "unsupported_msg_type",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg:    &authz.MsgGrant{},
+			expErr: sdkerrors.ErrInvalidType,
+		},
+		{
+			name:   "account_deposit_deployment_scope",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.DeploymentID(t).ToEscrowAccountID(),
+				dep(sdk.NewInt64Coin(denom, 500)),
+			),
+			expAccept:  true,
+			expUpdated: true,
+		},
+		{
+			name:   "account_deposit_bid_scope",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeBid},
+			limits: sdk.Coins{limit},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.BidID(t).ToEscrowAccountID(),
+				dep(sdk.NewInt64Coin(denom, 500)),
+			),
+			expAccept:  true,
+			expUpdated: true,
+		},
+		{
+			name:   "account_deposit_invalid_scope",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg: func() sdk.Msg {
+				msg := v1.NewMsgAccountDeposit(
+					testutil.AccAddress(t).String(),
+					testutil.DeploymentID(t).ToEscrowAccountID(),
+					dep(sdk.NewInt64Coin(denom, 500)),
+				)
+				msg.ID.Scope = 99
+				return msg
+			}(),
+			expErr: module.ErrUnauthorizedDepositScope,
+		},
+		{
+			name:   "account_deposit_scope_not_authorized",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeBid},
+			limits: sdk.Coins{limit},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.DeploymentID(t).ToEscrowAccountID(),
+				dep(sdk.NewInt64Coin(denom, 500)),
+			),
+			expErr: module.ErrUnauthorizedDepositScope,
+		},
+		{
+			name:   "create_deployment_msg",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg: &dvbeta.MsgCreateDeployment{
+				Deposit: dep(sdk.NewInt64Coin(denom, 300)),
+			},
+			expAccept:  true,
+			expUpdated: true,
+		},
+		{
+			name:   "create_deployment_scope_not_authorized",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeBid},
+			limits: sdk.Coins{limit},
+			msg: &dvbeta.MsgCreateDeployment{
+				Deposit: dep(sdk.NewInt64Coin(denom, 300)),
+			},
+			expErr: module.ErrUnauthorizedDepositScope,
+		},
+		{
+			name:   "create_bid_msg",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeBid},
+			limits: sdk.Coins{limit},
+			msg: &mvbeta.MsgCreateBid{
+				Deposit: dep(sdk.NewInt64Coin(denom, 300)),
+			},
+			expAccept:  true,
+			expUpdated: true,
+		},
+		{
+			name:   "create_bid_scope_not_authorized",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg: &mvbeta.MsgCreateBid{
+				Deposit: dep(sdk.NewInt64Coin(denom, 300)),
+			},
+			expErr: module.ErrUnauthorizedDepositScope,
+		},
+		{
+			name:   "denom_not_in_limits",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.DeploymentID(t).ToEscrowAccountID(),
+				dep(sdk.NewInt64Coin(sdkutil.DenomUakt, 500)),
+			),
+			expAccept: false,
+		},
+		{
+			name:   "exact_limit_deletes_authorization",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.DeploymentID(t).ToEscrowAccountID(),
+				dep(limit),
+			),
+			expAccept:  true,
+			expDelete:  true,
+			expUpdated: true,
+		},
+		{
+			name:   "exceeds_limit_no_partial",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.DeploymentID(t).ToEscrowAccountID(),
+				dep(sdk.NewInt64Coin(denom, 1001)),
+			),
+			partial: false,
+			expErr:  sdkerrors.ErrInsufficientFunds,
+		},
+		{
+			name:   "exceeds_limit_with_partial",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{limit},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.DeploymentID(t).ToEscrowAccountID(),
+				dep(sdk.NewInt64Coin(denom, 1500)),
+			),
+			partial:    true,
+			expAccept:  true,
+			expDelete:  true,
+			expUpdated: true,
+		},
+		{
+			name:   "legacy_spend_limit_migration",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			limits: sdk.Coins{sdk.NewInt64Coin(denom, 500)},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.DeploymentID(t).ToEscrowAccountID(),
+				dep(sdk.NewInt64Coin(denom, 800)),
+			),
+		},
+		{
+			name:   "multi_denom_limits",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment, v1.DepositScopeBid},
+			limits: sdk.Coins{sdk.NewInt64Coin(denom, 500), sdk.NewInt64Coin(sdkutil.DenomUakt, 300)},
+			msg: v1.NewMsgAccountDeposit(
+				testutil.AccAddress(t).String(),
+				testutil.DeploymentID(t).ToEscrowAccountID(),
+				dep(sdk.NewInt64Coin(sdkutil.DenomUakt, 200)),
+			),
+			expAccept:  true,
+			expUpdated: true,
+		},
+	}
 
-	// Send the wrong type of message, expect an error
-	var msg sdk.Msg
-	response, err := dda.Accept(sctx, msg)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "invalid type")
-	require.Zero(t, response)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			var da *v1.DepositAuthorization
 
-	// Try to deposit too much coin, expect an error
-	spendReq := limit.Add(sdk.NewInt64Coin(sdkutil.DenomUact, 1))
+			if tt.name == "legacy_spend_limit_migration" {
+				da = &v1.DepositAuthorization{
+					Scopes:      tt.scopes,
+					SpendLimit:  sdk.NewInt64Coin(denom, 500),
+					SpendLimits: tt.limits,
+				}
+				resp, err := da.TryAccept(sdk.Context{}, tt.msg, tt.partial)
+				require.NoError(t, err)
+				require.True(t, resp.Accept)
+				return
+			}
 
-	did := testutil.DeploymentID(t)
+			da = v1.NewDepositAuthorization(tt.scopes, tt.limits)
 
-	msg = v1.NewMsgAccountDeposit(did.Owner, did.ToEscrowAccountID(), deposit.Deposit{
-		Amount:  spendReq,
-		Sources: deposit.Sources{deposit.SourceGrant},
-	})
+			resp, err := da.TryAccept(sdk.Context{}, tt.msg, tt.partial)
 
-	response, err = dda.Accept(sctx, msg)
-	require.ErrorIs(t, err, sdkerrors.ErrInsufficientFunds)
-	require.Zero(t, response)
+			if tt.expErr != nil {
+				require.ErrorIs(t, err, tt.expErr)
+				if tt.expErrMsg != "" {
+					require.Contains(t, err.Error(), tt.expErrMsg)
+				}
+				return
+			}
 
-	did = testutil.DeploymentID(t)
-	// Deposit 1 less than the limit, expect an updated deposit
-	msg = v1.NewMsgAccountDeposit(did.Owner, did.ToEscrowAccountID(), deposit.Deposit{
-		Amount:  limit.Sub(sdk.NewInt64Coin(sdkutil.DenomUact, 1)),
-		Sources: deposit.Sources{deposit.SourceGrant},
-	})
-	response, err = dda.Accept(sctx, msg)
-	require.NoError(t, err)
-	require.True(t, response.Accept)
-	require.False(t, response.Delete)
+			require.NoError(t, err)
+			require.Equal(t, tt.expAccept, resp.Accept)
 
-	ok := false
-	dda, ok = response.Updated.(*v1.DepositAuthorization)
-	require.True(t, ok)
+			if !tt.expAccept {
+				return
+			}
 
-	did = testutil.DeploymentID(t)
-	// Deposit the limit (now 1), expect that it is not to be deleted
-	msg = v1.NewMsgAccountDeposit(did.Owner, did.ToEscrowAccountID(), deposit.Deposit{
-		Amount:  sdk.NewInt64Coin(sdkutil.DenomUact, 1),
-		Sources: deposit.Sources{deposit.SourceGrant},
-	})
-	response, err = dda.Accept(sctx, msg)
-	require.NoError(t, err)
-	require.True(t, response.Accept)
-	require.True(t, response.Delete)
+			require.Equal(t, tt.expDelete, resp.Delete)
+
+			if tt.expUpdated {
+				updated, ok := resp.Updated.(*v1.DepositAuthorization)
+				require.True(t, ok)
+				require.Equal(t, tt.scopes, updated.Scopes)
+				require.True(t, updated.SpendLimit.Amount.Equal(sdkmath.ZeroInt()))
+			}
+		})
+	}
+}
+
+func TestDepositAuthorization_ValidateBasic(t *testing.T) {
+	denom := sdkutil.DenomUact
+
+	tests := []struct {
+		name      string
+		auth      *v1.DepositAuthorization
+		expErr    error
+		expErrMsg string
+	}{
+		{
+			name: "valid_single_scope",
+			auth: v1.NewDepositAuthorization(
+				v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+				sdk.Coins{sdk.NewInt64Coin(denom, 100)},
+			),
+		},
+		{
+			name: "valid_multiple_scopes",
+			auth: v1.NewDepositAuthorization(
+				v1.DepositAuthorizationScopes{v1.DepositScopeDeployment, v1.DepositScopeBid},
+				sdk.Coins{sdk.NewInt64Coin(denom, 100)},
+			),
+		},
+		{
+			name: "empty_scopes",
+			auth: v1.NewDepositAuthorization(
+				v1.DepositAuthorizationScopes{},
+				sdk.Coins{sdk.NewInt64Coin(denom, 100)},
+			),
+			expErr:    module.ErrInvalidAuthzScope,
+			expErrMsg: "empty scope",
+		},
+		{
+			name: "invalid_scope_value",
+			auth: v1.NewDepositAuthorization(
+				v1.DepositAuthorizationScopes{v1.DepositScopeInvalid},
+				sdk.Coins{sdk.NewInt64Coin(denom, 100)},
+			),
+			expErr:    module.ErrInvalidAuthzScope,
+			expErrMsg: "invalid scope",
+		},
+		{
+			name: "unknown_scope_value",
+			auth: v1.NewDepositAuthorization(
+				v1.DepositAuthorizationScopes{99},
+				sdk.Coins{sdk.NewInt64Coin(denom, 100)},
+			),
+			expErr:    module.ErrInvalidAuthzScope,
+			expErrMsg: "invalid scope",
+		},
+		{
+			name: "duplicate_scopes",
+			auth: v1.NewDepositAuthorization(
+				v1.DepositAuthorizationScopes{v1.DepositScopeDeployment, v1.DepositScopeDeployment},
+				sdk.Coins{sdk.NewInt64Coin(denom, 100)},
+			),
+			expErr:    module.ErrInvalidAuthzScope,
+			expErrMsg: "duplicate scope",
+		},
+		{
+			name: "invalid_spend_limit",
+			auth: &v1.DepositAuthorization{
+				Scopes:     v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+				SpendLimit: sdk.Coin{Denom: denom, Amount: sdkmath.NewInt(-1)},
+			},
+			expErr:    sdkerrors.ErrInvalidCoins,
+			expErrMsg: "spend limit is not valid",
+		},
+		{
+			name: "invalid_spend_limits",
+			auth: &v1.DepositAuthorization{
+				Scopes:      v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+				SpendLimit:  sdk.NewCoin(sdkutil.DenomUakt, sdkmath.ZeroInt()),
+				SpendLimits: sdk.Coins{sdk.Coin{Denom: denom, Amount: sdkmath.NewInt(-1)}},
+			},
+			expErr:    sdkerrors.ErrInvalidCoins,
+			expErrMsg: "spend limits are not valid",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.auth.ValidateBasic()
+
+			if tt.expErr != nil {
+				require.ErrorIs(t, err, tt.expErr)
+				if tt.expErrMsg != "" {
+					require.Contains(t, err.Error(), tt.expErrMsg)
+				}
+				return
+			}
+
+			require.NoError(t, err)
+		})
+	}
+}
+
+func TestDepositAuthorizationScopes_Has(t *testing.T) {
+	tests := []struct {
+		name   string
+		scopes v1.DepositAuthorizationScopes
+		val    v1.DepositAuthorization_Scope
+		exp    bool
+	}{
+		{
+			name:   "has_deployment",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment, v1.DepositScopeBid},
+			val:    v1.DepositScopeDeployment,
+			exp:    true,
+		},
+		{
+			name:   "has_bid",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment, v1.DepositScopeBid},
+			val:    v1.DepositScopeBid,
+			exp:    true,
+		},
+		{
+			name:   "missing_scope",
+			scopes: v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+			val:    v1.DepositScopeBid,
+			exp:    false,
+		},
+		{
+			name:   "empty_scopes",
+			scopes: v1.DepositAuthorizationScopes{},
+			val:    v1.DepositScopeDeployment,
+			exp:    false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			require.Equal(t, tt.exp, tt.scopes.Has(tt.val))
+		})
+	}
+}
+
+func TestDepositAuthorization_MsgTypeURL(t *testing.T) {
+	da := v1.NewDepositAuthorization(
+		v1.DepositAuthorizationScopes{v1.DepositScopeDeployment},
+		sdk.Coins{sdk.NewInt64Coin(sdkutil.DenomUact, 100)},
+	)
+	url := da.MsgTypeURL()
+	require.NotEmpty(t, url)
+	require.Contains(t, url, "MsgAccountDeposit")
 }
